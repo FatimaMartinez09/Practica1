9beaa6cc7934717d2ef2e4ec1d8c3113515066a3
FatimaMartinez09 <42728508+FatimaMartinez09@users.noreply.github.com>
2018-09-08 09:12:19 -0600

    Add files via upload

---
+++ src/main/java/com/example/hv12/ejercicio_1_temporizador/Cargar.java
@@ -0,0 +1,11 @@
+package com.example.hv12.ejercicio_1_temporizador;
+
+import android.os.AsyncTask;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class Cargar extends AsyncTask<Integer, Integer, String>{
+    Context context;
+    ProgressBar barra;
+    TextView lblValor;
+}
